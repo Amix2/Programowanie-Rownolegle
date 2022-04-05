7d99924c2b3a0f3f4b185fb3e53cc88e81911fe2
Dima <zhylko.dima@gmail.com>
2022-04-05 05:12:25 +0200

    rename y axis on 2 plots

--- OMP/Plot.m
+++ OMP/Plot.m
@@ -1,57 +1,57 @@
 Small = {
-    'Data\small_0.txt'
-    %'Data\small_1.txt'
-    'Data\small_2.txt'
-    'Data\small_3.txt'
-    'Data\small_4.txt'
-    'Data\small_5.txt'
-    'Data\small_6.txt'
-    'Data\small_7.txt'
-    'Data\small_8.txt'
-    'Data\small_9.txt'
-'Data\small_10.txt'
+    'Data/small_0.txt'
+    %'Data/small_1.txt'
+    'Data/small_2.txt'
+    'Data/small_3.txt'
+    'Data/small_4.txt'
+    'Data/small_5.txt'
+    'Data/small_6.txt'
+    'Data/small_7.txt'
+    'Data/small_8.txt'
+    'Data/small_9.txt'
+'Data/small_10.txt'
 };
 
 Med1 = {
-    %'Data\med1_0.txt'
-    %'Data\med1_1.txt'
-    'Data\med1_2.txt'
-    'Data\med1_3.txt'
-    'Data\med1_4.txt'
-    'Data\med1_5.txt'
-    'Data\med1_6.txt'
-    'Data\med1_7.txt'
-    'Data\med1_8.txt'
-    'Data\med1_9.txt'
-%'Data\med1_10.txt'
+    %'Data/med1_0.txt'
+    %'Data/med1_1.txt'
+    'Data/med1_2.txt'
+    'Data/med1_3.txt'
+    'Data/med1_4.txt'
+    'Data/med1_5.txt'
+    'Data/med1_6.txt'
+    'Data/med1_7.txt'
+    'Data/med1_8.txt'
+    'Data/med1_9.txt'
+%'Data/med1_10.txt'
 };
 
 Med2 = {
-    'Data\med2_0.txt'
-    'Data\med2_1.txt'
-    'Data\med2_2.txt'
-    'Data\med2_3.txt'
-    'Data\med2_4.txt'
-    'Data\med2_5.txt'
-    'Data\med2_6.txt'
-    'Data\med2_7.txt'
-    'Data\med2_8.txt'
-    'Data\med2_9.txt'
- 'Data\med2_10.txt'
+    'Data/med2_0.txt'
+    'Data/med2_1.txt'
+    'Data/med2_2.txt'
+    'Data/med2_3.txt'
+    'Data/med2_4.txt'
+    'Data/med2_5.txt'
+    'Data/med2_6.txt'
+    'Data/med2_7.txt'
+    'Data/med2_8.txt'
+    'Data/med2_9.txt'
+ 'Data/med2_10.txt'
 };
 
 Big = {
-    'Data\big_0.txt'
-    'Data\big_1.txt'
-    'Data\big_2.txt'
-    'Data\big_3.txt'
-    'Data\big_4.txt'
-    'Data\big_5.txt'
-    'Data\big_6.txt'
-    'Data\big_7.txt'
-    'Data\big_8.txt'
-    'Data\big_9.txt'
-'Data\big_10.txt'
+    'Data/big_0.txt'
+    'Data/big_1.txt'
+    'Data/big_2.txt'
+    'Data/big_3.txt'
+    'Data/big_4.txt'
+    'Data/big_5.txt'
+    'Data/big_6.txt'
+    'Data/big_7.txt'
+    'Data/big_8.txt'
+    'Data/big_9.txt'
+'Data/big_10.txt'
 };
 
 Small = CombineResults(Small, false);
@@ -93,7 +93,7 @@ function ShowPlot(Data, Name)
     plot(Xs, Data(:,6));
     plot(Xs, Data(:,7));
     xlabel ("Liczba wątków");
-    ylabel ("Czas wykonania [ms]");
+    ylabel ("Przyspieszenie [ms]");
     title ("Przyspieszenie - " +Name);
     legend({'static 1', 'static 1e4', 'dynamic 1', 'dynamic 1e4', 'guided 1', 'guided 1e4'},'Location','northwest')
     hold off
@@ -110,7 +110,7 @@ function ShowPlot(Data, Name)
     plot(Xs, Data(:,6));
     plot(Xs, Data(:,7));
     xlabel ("Liczba wątków");
-    ylabel ("Czas wykonania [ms]");
+    ylabel ("Efektywność [%]");
     title ("Efektywność - " +Name);
     legend({'static 1', 'static 1e4', 'dynamic 1', 'dynamic 1e4', 'guided 1', 'guided 1e4'},'Location','southwest')
     hold off
